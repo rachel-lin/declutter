65dc6c3d423e600cfca7ff8549b6f21226eb4e42
Rachel Lin <rachel.lin91@gmail.com>
2015-05-29 02:58:34 +0800

    added delete function

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -4,17 +4,32 @@ class ItemsController < ApplicationController
       @item = Item.new
   end
 
+  def index
+    @items = Item.all
+  end
+
 
       def create
       @item = Item.new(item_params)
         if @item.save
           flash[:notice] = "Item was saved."
-          redirect_to @item
+          render @items
         else 
           flash[:error] = "There was an error saving your item. Please try again."
           render :new
         end
       end
+
+def destroy
+     @item = Item.find(params[:id])
+ 
+          if @item.destroy
+            flash[:notice] = "Item was deleted."
+            render :index
+          end
+   end
+
+
 
   private
 
