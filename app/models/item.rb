69eb28cc996f78cf82f38ad5d0696ac958dc3d61
Rachel Lin <rachel.lin91@gmail.com>
2015-06-03 01:18:10 +0800

    added days left counter

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,6 +2,6 @@ class Item < ActiveRecord::Base
   belongs_to :user
 
   def days_left
-    (DateTime.now.to_date - created_at.to_date).to_i
+    7 - (DateTime.now.to_date - created_at.to_date).to_i
   end
 end
